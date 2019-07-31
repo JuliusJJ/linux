b5dee9ac319e874772478f71225abf5ac4dcc21f
JuliusJJ <43318628+JuliusJJ@users.noreply.github.com>
2019-07-31 14:54:08 +0300

    Update testas.php

--- testas.php
+++ testas.php
@@ -72,8 +72,8 @@ foreach ($klubai as $k) {
     mysqli_close($conn);
 
     // putenv("PHANTOMJS_EXECUTABLE=node_modules/phantomjs/lib/phantom/bin/phantomjs.exe");
-    exec('casperjs scrape_members.js "' . $settings["conName"] . '" "' . $settings["conPsw"] . '" "' . $id . '"');
-    exec('casperjs scrape_guests.js "' . $settings["conName"] . '" "' . $settings["conPsw"] . '" "' . $id . '"');
+    exec('casperjs /downloads/linux/scrape_members.js "' . $settings["conName"] . '" "' . $settings["conPsw"] . '" "' . $id . '"');
+    exec('casperjs /downloads/linux/scrape_guests.js "' . $settings["conName"] . '" "' . $settings["conPsw"] . '" "' . $id . '"');
 
     if ($json1 = file_get_contents('./' . $id . 'members.json')) {
         echo $id;
